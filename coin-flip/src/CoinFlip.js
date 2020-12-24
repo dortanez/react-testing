2ca4285513b4401973aeeb1dacf364daa452a00a
Danisa Ortanez <dortanez@yahoo.com>
2020-12-23 20:34:00 -0800

    testing

---
+++ coin-flip/src/CoinFlip.js
@@ -0,0 +1,35 @@
+import {React, useState} from 'react';
+import Coin from './Coin';
+import front from './front.jpg';
+import back from './back.jpg';
+import './CoinFlip.css';
+
+const CoinFlip = (props) => {
+    const [image, setImage] = useState('');
+    const [heads, setHeads] = useState(0);
+    const [tails, setTails] = useState(0);
+
+    const flipCoin = () => {
+        const random = Math.floor(Math.random() * 2);
+        const choice = props.coin[random];
+        setImage(image => choice);
+
+        choice === front ? setHeads(heads+1) : setTails(tails+1);
+    }
+
+    return (
+        <div className='CoinFlip'>
+            <h1>Let's flip a coin!</h1>
+            <img src={image}></img>
+            <button onClick={flipCoin}>Flip Me</button>
+            <p>{`Out of ${heads + tails} flips, there have been ${heads} heads and ${tails} tails.`}</p>
+        </div>
+        
+    )
+}
+
+CoinFlip.defaultProps = {
+    coin: [front, back]
+}
+
+export default CoinFlip;
---
+++ coin-flip/src/CoinFlip.test.js
@@ -0,0 +1,10 @@
+import { render, fireEvent } from '@testing-library/react';
+import CoinFlip from './CoinFlip';
+
+it('should render without crashing',() => {
+    render(<CoinFlip/>)
+})
+
+it('should not show coin image when page loads',() => {
+    const 
+})
